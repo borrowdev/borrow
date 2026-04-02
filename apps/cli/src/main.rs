d939ee35be05e6d390a406e809a859cf4a5e8eec
Lorenzo Bloedow <me@lorenzobloedow.com>
2026-04-01 21:22:58 -0300

    feat(ms): Add 'ms' subcommand to Borrow CLI

--- apps/cli/src/main.rs
+++ apps/cli/src/main.rs
@@ -1,27 +1,31 @@
+mod ms;
 mod start;
-use start::{handle_start_command, StartCommand};
 use clap::{Parser, Subcommand};
+use ms::{MsCommand, handle_ms_command};
+use start::{StartCommand, handle_start_command};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, arg_required_else_help(true))]
 struct Cli {
     #[command(subcommand)]
-    command: Command
+    command: Command,
 }
 
 #[derive(Subcommand, Debug)]
 enum Command {
     /// Scaffold projects and components with Borrow Start.
     #[command(name = "start", subcommand, arg_required_else_help(true))]
-    StartCommand(StartCommand)
+    StartCommand(StartCommand),
+
+    /// Measure the latency of your API around the world
+    #[command(name = "ms")]
+    Ms(MsCommand),
 }
 
 fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Command::StartCommand(start_command) => {
-            handle_start_command(start_command)
-        }
+        Command::StartCommand(start_command) => handle_start_command(start_command),
+        Command::Ms(ms_command) => handle_ms_command(ms_command),
     }
 }
-
